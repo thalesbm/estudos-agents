2a81eaa11bc1ba4de3874e79543791c2b03717ed
Thales Marega <thalesmarega@Thaless-MacBook-Pro.local>
2025-07-07 22:54:38 -0300

    refactor code

--- caso-de-uso/model/enum/connection_type.py
+++ caso-de-uso/model/enum/connection_type.py
@@ -3,6 +3,4 @@ from enum import Enum
 class ConnectionType(Enum):
     BASIC_CONNECTION = "conexao-simples-llm"
     CONNECTION_WITH_TOOLS = "conexao-com-tool"
-
-    def __getitem__(self, name):
-        return super().__getitem__(name)
+    CONNECTION_WITH_TOOLS_AND_REACT = "conexao-com-tool-react"
--- caso-de-uso/service/agent_tools/prompt.py
+++ caso-de-uso/service/agent_tools/prompt.py
@@ -36,27 +36,3 @@ class Prompt:
         ])
 
         return prompt
-    
-    def get_react_prompt():
-        prompt = ChatPromptTemplate.from_messages([
-            (
-                "system",
-                "Você é um assistente que universitario que retorna as informações de forma clara e objetiva"
-                "Sempre explique seu raciocínio (Thought), execute uma ação (Action) e observe o resultado (Observation). "
-                "Finalize apenas quando tiver uma resposta completa."
-                "Caso o usuário pergunte sobre os modelos de celulares que são utilizados no brasil, chame a função celulares_atualizados() e retorne o resultado"
-                "{context}"
-                "{agent_scratchpad}"
-            ),
-            (
-                "user",
-                "{query}"
-            )
-            # ,
-            # (
-            #     "human",
-            #     "Pergunta: {query} Responda de forma clara e cite a fonte se possível."
-            # )
-        ])
-
-        return prompt
